e7301cd89246ed478c281096ada34dbddb20abf0
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-03 14:12:12 +0200

    generative cards

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import React from "react";
 // Components
 import NavBar from "./comp/nav-bar";
 import HeroSection from "./comp/hero-section";
-import Card from "./comp/card"
+import Card from "./comp/card";
 
+// Import data
+import data from "./data";
+
+let appCards = data.map((item) => (
+  <Card
+    img={item.coverImg}
+    rating={item.stats.rating}
+    reviewCount={item.stats.reviewCount}
+    title={item.title}
+    price={item.price}
+  />
+));
 
 const App = () => (
-
   <div>
     <NavBar />
-    <HeroSection/>
-    <Card 
-        img="man.png" 
-        rating = "5.0"
-        reviewCount = "6"
-        title = "Life Lessons with Katie Zaferes"
-        price = "136"
-    />
+    <HeroSection />
+    <div className="card--list">{appCards}</div>
   </div>
 );
 
